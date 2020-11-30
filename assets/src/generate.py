08704dc48f62b57e9d8836033aa329349acc8e26
mzyzc <mzyzc@czyz.xyz>
2020-11-30 08:12:40 +0000

    Graph generation script now always looks in the correct directory

--- assets/src/generate.py
+++ assets/src/generate.py
@@ -5,7 +5,7 @@ import os
 initial_dir = os.getcwd()
 final_dir = os.path.dirname(initial_dir)
 
-for filename in os.listdir(os.getcwd()):
+for filename in os.listdir(initial_dir):
     if filename.endswith('.mmd'):
         initial_file = os.path.join(initial_dir, filename)
         final_file = os.path.join(final_dir, filename.replace('.mmd', '.png'))
